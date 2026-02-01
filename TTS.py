428b1765817e663cc18ef2704661d13d8e6ca258
Tino Nonnast <nonnasttino@outlook.com>
2026-02-01 22:45:03 +0100

    Update

--- TTS.py
+++ TTS.py
@@ -1,15 +1,18 @@
 from gtts import gTTS
 import pygame
 import time
+import os
 
 def TTS_play(text, language):
     tts = gTTS(text, lang=language) # language e.g.: (I think) de | en 
-    tts.save("output.mp3")
+    tts.save("audio/OUTPUT.mp3")
     pygame.mixer.init()
-    pygame.mixer.music.load("output.mp3")
+    pygame.mixer.music.load("audio/OUTPUT.mp3")
     pygame.mixer.music.play()
 
     while pygame.mixer.music.get_busy():
         time.sleep(0.1)
+
+    # os.remove("audio/OUTPUT.mp3")
 
     return
--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import TTS
 
 lang_in = "en-US" # e.g. en-US | de-DE
 lang_out = "de" # e.g. de | en | cn
+use_ai = False
+wav_file = "audio/INPUT.wav"
 
-
-transcribe_done = transcribe.Recognize_Speech(wav_file="INPUT.wav", language=lang_in)
+transcribe_done = transcribe.Recognize_Speech(wav_file=wav_file, language=lang_in)
 print(transcribe_done)
-# translation = translation_ai.Translate_AI(text=transcribe_done, language=lang_out)
-translation = translation_api.Translate_API(text=transcribe_done, language=lang_out)
+if use_ai:
+    translation = translation_ai.Translate_AI(text=transcribe_done, language=lang_out)
+else:
+    translation = translation_api.Translate_API(text=transcribe_done, language=lang_out)
+
 print(translation)
 TTS.TTS_play(text = translation, language=lang_out)
